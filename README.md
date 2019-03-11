bb1c9e10d549b90fe781a0bfb359e17352009d4c
Dehru Cromer <dehru.cromer@salesforce.com>
2019-03-11 16:41:26 -0600

    example vscode extension dealing with right click context

---
+++ README.md
@@ -0,0 +1,14 @@
+# helloworld README
+
+This is just a test extension to see if I can publish it to theia
+
+## Features
+
+This extension will also run in theia
+
+## Release Notes
+
+First release is just hello world
+
+### 0.0.1
+First release is just hello world
---
+++ src/extension.ts
@@ -0,0 +1,69 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+		console.log('Congratulations, your extension "helloworld" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable1 = vscode.commands.registerCommand('extension.hello', () => {
+		// The code you place here will be executed every time your command is executed
+		console.log('displaying Hello');
+
+		let message = 'Hello!';
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage(message);
+	});
+
+	context.subscriptions.push(disposable1);
+
+	let disposable2 = vscode.commands.registerCommand('extension.world', () => {
+		// The code you place here will be executed every time your command is executed
+		console.log('displaying world');
+
+		let message = 'World!';
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage(message);
+	});
+
+	context.subscriptions.push(disposable2);
+
+	let disposable3 = vscode.commands.registerCommand('extension.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		console.log('displaying hello world');
+
+		let message = 'Hello World!';
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage(message);
+	});
+
+	context.subscriptions.push(disposable3);
+
+	let disposable4 = vscode.commands.registerCommand('extension.helloWorldPalette', () => {
+		// The code you place here will be executed every time your command is executed
+		console.log('displaying hello world');
+
+		let message = 'Hello World!';
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage(message);
+	});
+
+	context.subscriptions.push(disposable4);
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+	console.log('deactivated');
+}
